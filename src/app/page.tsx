1d29cadd5a681f786f0420c111853cef0ad04836
somdobomk <somdobomk@gmail.com>
2023-01-26 22:16:42 -0600

    fix search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,49 +17,45 @@ export default function Home() {
 		setLoading(true);
 		axios.get(url).then((res) => {
 			setWeather(res.data);
-			console.log(res.data);
 		});
 		setCity('');
 		setLoading(false);
 	};
 
-		return (
-			<div>
-				{/* Overlay */}
-				<div className="absolute top-0 left-0 right-0 bottom-0 z-[1] bg-black/60" />
+	return (
+		<div>
+			{/* Overlay */}
+			<div className="absolute top-0 left-0 right-0 bottom-0 z-[1] bg-black/60" />
 
-				{/* Background Image */}
-				<Image
-					className="object-cover"
-					src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1217&q=80"
-					alt="Picture of the weather"
-					fill={true}
-				/>
+			{/* Background Image */}
+			<Image
+				className="object-cover"
+				src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1217&q=80"
+				alt="Picture of the weather"
+				fill={true}
+			/>
 
-				{/* Search */}
-				<div className="relative z-10 m-auto flex w-full max-w-[500px] items-center justify-between pt-4 text-white">
-					<form
-						onSubmit={getWeather}
-						className="m-auto flex w-full items-center justify-between rounded-2xl border border-gray-300 bg-transparent p-3 text-white">
-						<div>
-							<input
-								onChangeCapture={(e) => setCity(e.target.value)}
-								className="border-none bg-transparent text-2xl text-white focus:outline-none"
-								type={Text}
-								value={city}
-								placeholder="Search City"
-								onChange={(e) => setCity(e.target.value)}
-							/>
-						</div>
-						<button onClick={getWeather}>
-							<BsSearch size={20} />
-						</button>
-					</form>
-				</div>
-
-				{/* Weather */}
-				{weather.main && <Weather data={weather} />}
+			{/* Search */}
+			<div className="relative z-10 m-auto flex w-full max-w-[500px] items-center justify-between pt-4 text-white">
+				<form
+					onSubmit={getWeather}
+					className="m-auto flex w-full items-center justify-between rounded-2xl border border-gray-300 bg-transparent p-3 text-white">
+					<div>
+						<input
+							onChange={(e) => setCity(e.target.value)}
+							className="border-none bg-transparent text-2xl text-white focus:outline-none"
+							type="text"
+							placeholder="Search City"
+						/>
+					</div>
+					<button onClick={getWeather}>
+						<BsSearch size={20} />
+					</button>
+				</form>
 			</div>
-		);
-	}
+
+			{/* Weather */}
+			{weather.main && <Weather data={weather} />}
+		</div>
+	);
 }
